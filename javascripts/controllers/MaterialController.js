9603658073172617e4c30cb0931ef976e58f506d
etienne <etienne.gautier@aol.fr>
2015-05-18 10:22:46 +0200

    resolution liens

--- javascripts/controllers/MaterialController.js
+++ javascripts/controllers/MaterialController.js
@@ -1,8 +1,8 @@
 var app = angular.module('StarterApp', ['ngMaterial']);
 
 app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-  $scope.toggleSidenav = function(menuId) {
-    $mdSidenav(menuId).toggle();
-  };
- 
+    $scope.toggleSidenav = function(menuId) {
+        $mdSidenav(menuId).toggle();
+    };
+
 }]);
