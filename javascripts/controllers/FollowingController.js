5ab4d3b8fc7592f8fecbe1666cf536a24e68d781
Charles <oxachon@hotmail.fr>
2015-06-01 23:46:08 +0200

    Correction bug ID user

--- javascripts/controllers/FollowingController.js
+++ javascripts/controllers/FollowingController.js
@@ -1,10 +1,10 @@
 /**
  * Created by Oxachon on 01/06/2015.
  */
-appControllers.controller('FollowingController', function($scope,$http) {
+appControllers.controller('FollowingController', function($scope,$http,$window) {
     var current_u = $window.sessionStorage.getItem("utilisateur");
     var user = JSON.parse(current_u);
-    $http.get(ooptions.api.base_url +"/user/"+ user.id+ "/following").success(function (data) {
+    $http.get(options.api.base_url +"/user/"+ user.id+ "/following").success(function (data) {
         console.log(data);
         $scope.users = data.users;
     })
